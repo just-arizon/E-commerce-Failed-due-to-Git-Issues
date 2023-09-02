a186dbfcd2b411cbcc582807d1b6969d93e23cde
Arinze <calistusarinzechinweuba@gmail.com>
2023-09-03 00:11:57 +0100

    Add register page

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 
-
+const users = []
 app.set('view-engine', 'ejs')
-
+app.use(express.urlencoded({ extended: false}))
 app.get('/', (req, res) => {
-    res.render('index.ejs', { name: 'Kyle'})
+    res.render('index.ejs')
 })
 
 app.get('/login', (req, res) => {
@@ -16,7 +16,12 @@ app.get('/register', (req, res) => {
     res.render('register.ejs')
 })
 
-app.post('/register', (req, res) =>{
-    
+app.post('/register', (req, res) => {
+    req.body.email
 })
+
+
+
 app.listen(3000)
+// middleware & static files
+app.use(express.static('public'));
